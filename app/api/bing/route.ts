13d0bcf713a0b7debfc9502f7d4d2f5bf3ed70de
Abe Hendricks <ahendricks@pcwa.net>
2024-05-15 09:27:26 -0700

    Bust cache workaround.

--- app/api/bing/route.ts
+++ app/api/bing/route.ts
@@ -1,10 +1,16 @@
 // app/api/bing/route.ts
-export const dynamic = "force-dynamic";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// this doesn't work, see https://github.com/vercel/next.js/issues/52350
+// export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
     const response = await fetch(
-      "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+      "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1",
+      {
+        cache: "no-store",
+      }
     );
     const data = await response.json();
     const imageUrl = `https://www.bing.com${data.images[0].url}`;
