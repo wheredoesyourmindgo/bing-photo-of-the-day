27055669a102f6f3f4e736f25813e35dc9fe0942
Abe Hendricks <ahendricks@pcwa.net>
2024-05-20 19:49:45 -0700

    ...

--- app/api/bing/route.ts
+++ app/api/bing/route.ts
@@ -1,15 +1,16 @@
 // app/api/bing/route.ts
 
-import { unstable_noStore } from "next/cache";
+// import { unstable_noStore } from "next/cache";
 
 // this doesn't work, see https://github.com/vercel/next.js/issues/52350
 // export const dynamic = "force-dynamic";
 
 export async function GET() {
-  unstable_noStore();
+  // unstable_noStore();
   try {
     const response = await fetch(
-      "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+      "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1",
+      { cache: "no-store" }
     );
     const data = await response.json();
     const imageUrl = `https://www.bing.com${data.images[0].url}`;
