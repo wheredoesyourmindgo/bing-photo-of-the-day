3b3de9cdd04c3650b24d67373d53c1fbc5c2d4f3
Abe Hendricks <ahendricks@pcwa.net>
2024-08-15 08:44:29 -0700

    Check for cron secret.

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,8 +2,13 @@
 
 import { revalidatePath } from "next/cache";
 
-export async function POST() {
+export async function POST(req: Request) {
   try {
+    if (
+      req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return new Response("Unauthorized", { status: 401 });
+    }
     revalidatePath("/");
     return new Response("Revalidation triggered", { status: 200 });
   } catch (error) {
