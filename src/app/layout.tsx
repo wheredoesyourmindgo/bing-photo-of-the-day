a17d3b1a59b5b624d3ed2b3953325ce125a00de3
Abe Hendricks <ahendricks@pcwa.net>
2026-01-19 09:35:28 -0800

    Add Inter font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import {Analytics} from '@vercel/analytics/next'
+import {Inter} from 'next/font/google'
 import './globals.css'
 
 export const metadata = {
@@ -6,10 +7,16 @@ export const metadata = {
   description: 'A sample Next.js app to display the Bing Photo of the Day'
 }
 
+const sansFont = Inter({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  display: 'swap' // or 'optional' to reduce visible swapping
+})
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${sansFont.variable}  font-sans antialiased`}>
         {children}
         <Analytics />
       </body>
